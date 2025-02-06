adf3df3c4a97d4d47067ed7dddc4f8efa565ee88
choihyerln <choi.hyehyehihi@gmail.com>
2025-02-06 19:27:16 +0900

    ✚ Chore: prettier, eslint, 개발의존성 파일 수정

--- 02. rest-countries-api-with-color-theme-switcher-master/eslint.config.js
+++ 02. rest-countries-api-with-color-theme-switcher-master/eslint.config.js
@@ -1,31 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import prettier from 'eslint-plugin-prettier';  // Prettier 플러그인 추가
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier"; // Prettier 플러그인 추가
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: "latest",
+      globals: globals.browser
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      "prettier": prettier,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: prettier
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }
       ],
-      "prettier/prettier": "error", // prettier의 포맷팅 에러를 eslint 에러 리포팅으로 볼 수 있도록 하는 규칙
-    },
-  },
-)
+      "prettier/prettier": [
+        "error",
+        {
+          trailingComma: "none" // JSX에서 쉼표가 강제되지 않도록 설정
+        }
+      ]
+    }
+  }
+);
