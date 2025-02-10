4afa7c99a58fc309e90ec140075c549b191fbda6
choihyerln <choi.hyehyehihi@gmail.com>
2025-02-11 03:30:02 +0900

    ♻️ Refactor: header a태그로 원복

--- 02. rest-countries-api-with-color-theme-switcher-master/src/layout/Header.tsx
+++ 02. rest-countries-api-with-color-theme-switcher-master/src/layout/Header.tsx
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom";
-
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 z-50 w-screen h-[68px] px-16 bg-white shadow-md shadow-gray-200 flex items-center">
-      <Link to={"/"}>
+      <a href="/">
         <h2>Where in the world?</h2>
-      </Link>
+      </a>
     </header>
   );
 }
