e190296a1076d6e6d88c6d197826e82a06f70bf4
choihyerln <choi.hyehyehihi@gmail.com>
2025-02-11 04:40:12 +0900

    💄 Design: `bg-color` default값 수정

--- 02. rest-countries-api-with-color-theme-switcher-master/src/pages/Home.tsx
+++ 02. rest-countries-api-with-color-theme-switcher-master/src/pages/Home.tsx
@@ -3,14 +3,16 @@ import SearchBox from "@/components/home/SearchBox";
 
 export default function Home() {
   return (
-    <div className={"w-[1425px] h-[791px] mt-[68px] px-16 py-10"}>
-      {/* 국가검색 & 지역필터 */}
-      <div className="flex justify-between px-15 mb-5">
-        <SearchBox />
-      </div>
+    <div className="bg-[hsl(0,0%,98%)]">
+      <div className={" w-[1425px] h-[791px] mt-[68px] px-16 py-10"}>
+        {/* 국가검색 & 지역필터 */}
+        <div className="flex justify-between px-15 mb-5">
+          <SearchBox />
+        </div>
 
-      {/* 국가 목록 */}
-      <CountryCardList />
+        {/* 국가 목록 */}
+        <CountryCardList />
+      </div>
     </div>
   );
 }
