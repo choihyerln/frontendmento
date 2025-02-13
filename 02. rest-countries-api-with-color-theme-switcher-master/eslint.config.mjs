9d270695145cd176e86f7eee8aae746a2ac83761
choihyerln <choi.hyehyehihi@gmail.com>
2025-02-13 17:55:40 +0900

    ✚ Chore: eslint 설정 수정

--- 02. rest-countries-api-with-color-theme-switcher-master/eslint.config.mjs
+++ 02. rest-countries-api-with-color-theme-switcher-master/eslint.config.mjs
@@ -1,4 +1,3 @@
-import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -6,31 +5,36 @@ import tseslint from "typescript-eslint";
 import prettier from "eslint-plugin-prettier"; // Prettier 플러그인 추가
 
 export default tseslint.config(
-  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['**/build/**', '**/dist/**', 'src/some/file/to/ignore.ts'],
+  },
+  tseslint.configs.recommended,
+  {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: "latest",
+      sourceType: 'module',
       globals: globals.browser
     },
     plugins: {
+      '@typescript-eslint': tseslint.plugin,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       prettier: prettier
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true }
-      ],
       "prettier/prettier": [
         "error",
         {
           trailingComma: "es5" // JSX에서 쉼표가 강제되지 않도록 설정
         }
-      ]
+      ],
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }
+      ],
+      "@typescript-eslint/no-unused-vars": "warn" // 사용하지 않는 변수 경고 표시
     }
   }
 );
