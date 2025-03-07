97220ec4efb6e9dd7238dc886c23e1e549b4117c
choihyerln <choi.hyehyehihi@gmail.com>
2025-03-07 19:40:47 +0900

    ✨ Feat: `Home` 영역 초안 구현

--- 03. sunnyside-agency-landing-page-main/src/pages/Home.tsx
+++ 03. sunnyside-agency-landing-page-main/src/pages/Home.tsx
@@ -1,11 +1,48 @@
-import ArrowDownIcon from "@/assets/images/icon-arrow-down.svg";
+import Egg from "@/assets/images/desktop/image-transform.jpg";
+import Brand from "@/components/home/BrandComponent";
+import Cup from "@/assets/images/desktop/image-stand-out.jpg";
+import Audience from "@/components/home/AudienceComponent";
+import Cherry from "@/assets/images/desktop/image-graphic-design.jpg";
+import Orange from "@/assets/images/desktop/image-photography.jpg";
+import Testimonials from "@/components/home/Testimonials";
+import Food from "@/components/home/FoodComponent";
+
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center p-8 mb-70 gap-30">
-      <h1 className="uppercase text-white tracking-[0.3em] text-center font-fraunces font-black text-[2.7rem]">
-        we are creatives
-      </h1>
-      <img className="" src={ArrowDownIcon} width={25} />
-    </div>
+    <>
+      <div className="grid md:grid-cols-2 grid-cols-1">
+        <Brand />
+        <img src={Egg} alt="Egg" className="w-full h-full" />
+        <img src={Cup} alt="Salmon colored cup" className=" w-full h-full" />
+        <Audience />
+        <div className="relative">
+          <img src={Cherry} alt="Cherry image" className="w-full h-full" />
+          <div className="absolute inset-0 top-1/2 flex-col p-4 md:p-20 text-darkcyan">
+            <h1 className="flex flex-col items-center text-sm md:text-3xl font-fraunces font-bold font-700 ">
+              Graphic Design
+            </h1>
+            <p className="text-xs font-bold pt-5 text-center px-10">
+              Great design makes you memorable. We deliver artwork that underscores your brand message and captures
+              potential clients’ attention.
+            </p>
+          </div>
+        </div>
+        <div className="relative">
+          <img src={Orange} alt="Orange image" className="w-full h-full" />
+          <div className="absolute inset-0 top-1/2 flex-col p-4 md:p-20 text-darkblue">
+            <h1 className="flex flex-col items-center text-sm md:text-3xl font-fraunces font-bold font-700 ">
+              Photography
+            </h1>
+            <p className="font-bold text-xs text-center pt-5 px-10">
+              Increase your credibility by getting the most stunning, high-quality photos that improve your business
+              image.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <Testimonials />
+      <Food />
+    </>
   );
 }
