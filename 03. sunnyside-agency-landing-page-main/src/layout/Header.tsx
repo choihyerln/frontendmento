933e837b1198f938934693f6fc5105952c738dbc
choihyerln <choi.hyehyehihi@gmail.com>
2025-03-07 17:56:10 +0900

    ♻️ Refactor: `Header` 영역 수정

--- 03. sunnyside-agency-landing-page-main/src/layout/Header.tsx
+++ 03. sunnyside-agency-landing-page-main/src/layout/Header.tsx
@@ -1,13 +1,29 @@
 import Logo from "@/components/layout/Logo";
 import NavMenu from "@/components/layout/NavMenu";
+import ArrowDownIcon from "@/assets/images/icon-arrow-down.svg";
 
-export default function Header() {
+const Header = () => {
   return (
-    <header>
+    <header
+      className="w-full h-auto bg-[url(/src/assets/images/desktop/image-header.jpg)] 
+    bg-cover bg-top absolute top-0 left-0"
+    >
+      {/* 상단바 */}
       <nav className="flex justify-between items-center h-30 p-10">
-        <Logo />
+        <a href="/" aria-label="Homepage" className="cursor-pointer">
+          <Logo />
+        </a>
         <NavMenu />
       </nav>
+
+      <div className="flex flex-col justify-center items-center p-8 mb-70 gap-30">
+        <h1 className="uppercase text-white tracking-[0.3em] text-center font-fraunces font-black text-[2.7rem]">
+          we are creatives
+        </h1>
+        <img src={ArrowDownIcon} width={25} />
+      </div>
     </header>
   );
-}
+};
+
+export default Header;
