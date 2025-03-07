260caa0491d7668f00f7505a118ca6ea7c78ac50
choihyerln <choi.hyehyehihi@gmail.com>
2025-03-07 19:40:00 +0900

    ✨ Feat: `Audience` 영역 컴포넌트 제작

---
+++ 03. sunnyside-agency-landing-page-main/src/components/home/AudienceComponent.tsx
@@ -0,0 +1,21 @@
+const Audience = () => {
+  return (
+    <div className="bg-[#FFFBF8] p-10 md:p-40 flex flex-col space-y-16">
+      <div className="w-1/2 p-20 font-fraunces font-black text-[hsl(212,27%,19%)] h-full flex flex-col justify-center">
+        <h1 className="text-4xl mb-4">Transform your brand</h1>
+        <p className="font-barlow font-semibold text-[hsl(213,9%,39%)]">
+          We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through
+          compelling visuals that do most of the marketing for you.
+        </p>
+        <p
+          className="uppercase mt-4 cursor-pointer
+           hover:underline hover:decoration-[hsl(51,100%,49%)] hover:decoration-8"
+        >
+          learn more
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Audience;
